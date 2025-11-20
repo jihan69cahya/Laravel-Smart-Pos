31a24dfd00a4eebc38c8e310b71d09c13b89a2e3
JihanCahya <jihan69cahya@gmail.com>
2025-11-20 20:55:12 +0700

    feat(migration): add migration tabel saldo awal

---
+++ database/migrations/2025_11_20_135122_create_table_saldo_awal.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_saldo_awal', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('tanggal');
+            $table->foreignId('id_produk')->constrained('m_produk')->onDelete('cascade');
+            $table->double('jumlah');
+            $table->foreignId('id_log_stok')->constrained('t_log_stok')->onDelete('restrict');
+            $table->text('keterangan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_saldo_awal');
+    }
+};
